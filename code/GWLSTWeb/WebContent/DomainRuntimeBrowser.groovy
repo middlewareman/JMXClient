cd2dfd977ebca1a9d9a4e8c17a79216ecd735fc0
Andreas <Andreas@9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9>
2010-12-14 18:04:16 +0000

    ExceptionHandler wrapping only
    git-svn-id: svn://localhost/GroovyJMX/trunk@101 9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9

--- code/GWLSTWeb/WebContent/DomainRuntimeBrowser.groovy
+++ code/GWLSTWeb/WebContent/DomainRuntimeBrowser.groovy
@@ -14,98 +14,101 @@ DomainRuntimeServer getDomainRuntimeServer() {
 	session?.domainRuntimeServer ?: DomainRuntimeServer.localRuntimeServer
 }
 
-if (params.objectName) {
-	def objectName = params.objectName
-	def home = domainRuntimeServer.home
-	assert home
-	def mbean = home.getMBean(objectName)
-	assert mbean
+new ExceptionHandler(binding).wrap {
 	
-	response.bufferSize = 350000
-	def htmlExporter = new HtmlExporter(response.writer)
-	
-	// TODO any additional parameters or preferences
-	
-	def timestamp = new Date()
-	def extras = [
-				'URL':request.requestURL,
-				'Timestamp':timestamp,
-				'user':request.remoteUser, 
-				'principal':request.userPrincipal]
-	htmlExporter.mbean mbean, extras
-	
-} else {
-	
-	def server = domainRuntimeServer
-	def service = server.domainRuntimeService
-	def adminServerName = service.DomainConfiguration.AdminServerName
-	def serverNames = service.ServerRuntimes.Name 
-	html.html {
-		head { title 'GWLST RuntimeMBeanServer Browser' }
-		body {
-			h1 'GWLST RuntimeMBeanServer Browser'
-			h2 'MBeanHome'
-			pre domainRuntimeServer.home
-			h2 'WebLogic Services'
-			for (name in [
-				'domainRuntimeService',
-				'typeService'
-			]) {
-				def mbean = server."$name"
-				def objectName = mbean.@objectName
-				h3 name
-				a(href:"?objectName=$objectName") { pre objectName }
-			}
-			
-			def selectedServerName = params.serverName ?: adminServerName
-			h2 "Java Platform MXBeans ($selectedServerName)"
-			def used = null
-			def enabled = null
-			try {
-				def jmx = domainRuntimeServer.domainRuntimeService.DomainConfiguration.JMX
-				enabled = jmx.PlatformMBeanServerEnabled
-				used = jmx.PlatformMBeanServerUsed	// Only from 10.3.3 ?
-			}
-			catch(Exception e) {
-				context.log "Could not get DomainConfiguration.JMX.PlatformMBeanServer{Enabled,Used}", e
-			}
-			if (!enabled && !used) {
-				i 'PlatformMBeanServer probably needs to be enabled in your domain configuration to see the beans below.'
-			}
-			
-			form {
-				select(name:'serverName') {
-					for (serverName in serverNames) {
-						if (serverName == selectedServerName) {
-							option selected:serverName, serverName
-						} else {
-							option serverName
+	if (params.objectName) {
+		def objectName = params.objectName
+		def home = domainRuntimeServer.home
+		assert home
+		def mbean = home.getMBean(objectName)
+		assert mbean
+		
+		response.bufferSize = 350000
+		def htmlExporter = new HtmlExporter(response.writer)
+		
+		// TODO any additional parameters or preferences
+		
+		def timestamp = new Date()
+		def extras = [
+					'URL':request.requestURL,
+					'Timestamp':timestamp,
+					'user':request.remoteUser, 
+					'principal':request.userPrincipal]
+		htmlExporter.mbean mbean, extras
+		
+	} else {
+		
+		def server = domainRuntimeServer
+		def service = server.domainRuntimeService
+		def adminServerName = service.DomainConfiguration.AdminServerName
+		def serverNames = service.ServerRuntimes.Name 
+		html.html {
+			head { title 'GWLST RuntimeMBeanServer Browser' }
+			body {
+				h1 'GWLST RuntimeMBeanServer Browser'
+				h2 'MBeanHome'
+				pre domainRuntimeServer.home
+				h2 'WebLogic Services'
+				for (name in [
+					'domainRuntimeService',
+					'typeService'
+				]) {
+					def mbean = server."$name"
+					def objectName = mbean.@objectName
+					h3 name
+					a(href:"?objectName=$objectName") { pre objectName }
+				}
+				
+				def selectedServerName = params.serverName ?: adminServerName
+				h2 "Java Platform MXBeans ($selectedServerName)"
+				def used = null
+				def enabled = null
+				try {
+					def jmx = domainRuntimeServer.domainRuntimeService.DomainConfiguration.JMX
+					enabled = jmx.PlatformMBeanServerEnabled
+					used = jmx.PlatformMBeanServerUsed	// Only from 10.3.3 ?
+				}
+				catch(Exception e) {
+					context.log "Could not get DomainConfiguration.JMX.PlatformMBeanServer{Enabled,Used}: e.message"
+				}
+				if (!enabled && !used) {
+					i 'PlatformMBeanServer probably needs to be enabled in your domain configuration to see the beans below.'
+				}
+				
+				form {
+					select(name:'serverName') {
+						for (serverName in serverNames) {
+							if (serverName == selectedServerName) {
+								option selected:serverName, serverName
+							} else {
+								option serverName
+							}
 						}
 					}
+					input type:'submit'
 				}
-				input type:'submit'
-			}
-			def map = server.getMBeanPlatformHome(selectedServerName).properties.findAll { key, value ->
-				value instanceof MBean || value instanceof Collection<MBean>
-			}
-			map.each { name, mbeans -> 
-				h3 name
-				if (mbeans instanceof MBean) {
-					def objectName = mbeans.@objectName
-					if (mbeans) {
-						a(href:"?objectName=$objectName") { pre objectName }
-					} else {
-						pre objectName
-					}
-				} else if (mbeans instanceof Collection<MBean>) {
-					ul {
-						mbeans.each { mbean ->
-							def objectName = mbean.@objectName
-							li {
-								if (mbean) {
-									a(href:"?objectName=$objectName") { pre objectName }
-								} else {
-									pre objectName
+				def map = server.getMBeanPlatformHome(selectedServerName).properties.findAll { key, value ->
+					value instanceof MBean || value instanceof Collection<MBean>
+				}
+				map.each { name, mbeans -> 
+					h3 name
+					if (mbeans instanceof MBean) {
+						def objectName = mbeans.@objectName
+						if (mbeans) {
+							a(href:"?objectName=$objectName") { pre objectName }
+						} else {
+							pre objectName
+						}
+					} else if (mbeans instanceof Collection<MBean>) {
+						ul {
+							mbeans.each { mbean ->
+								def objectName = mbean.@objectName
+								li {
+									if (mbean) {
+										a(href:"?objectName=$objectName") { pre objectName }
+									} else {
+										pre objectName
+									}
 								}
 							}
 						}
--- code/GWLSTWeb/WebContent/RuntimeBrowser.groovy
+++ code/GWLSTWeb/WebContent/RuntimeBrowser.groovy
@@ -14,81 +14,84 @@ RuntimeServer getRuntimeServer() {
 	session?.runtimeServer ?: RuntimeServer.localRuntimeServer
 }
 
-if (params.objectName) {
-	def home = runtimeServer.home
-	assert home
-	def mbean = home.getMBean(params.objectName)
-	assert mbean
+new ExceptionHandler(binding).wrap {
 	
-	response.bufferSize = 350000
-	def htmlExporter = new HtmlExporter(response.writer)	
-	
-	// TODO any additional parameters or preferences
-	
-	def timestamp = new Date()
-	def extras = [
-				'URL':request.requestURL,
-				'Timestamp':timestamp,
-				'user':request.remoteUser, 
-				'principal':request.userPrincipal]
-	htmlExporter.mbean mbean, extras
-	
-} else {
-	
-	html.html {
-		head { title 'GWLST RuntimeMBeanServer Browser' }
-		body {
-			h1 'GWLST RuntimeMBeanServer Browser'
-			h2 'MBeanHome'
-			pre runtimeServer.home
-			h2 'WebLogic Services'
-			for (name in [
-				'runtimeService',
-				'typeService'
-			]) {
-				def mbean = runtimeServer."$name"
-				def objectName = mbean.@objectName
-				h3 name
-				a(href:"?objectName=$objectName") { pre objectName }
-			}
-			
-			h2 'Java Platform MXBeans'
-			def used = null
-			def enabled = null
-			try {
-				def jmx = runtimeServer.runtimeService.DomainConfiguration.JMX
-				enabled = jmx.PlatformMBeanServerEnabled
-				used = jmx.PlatformMBeanServerUsed	// Only from 10.3.3 ?
-			}
-			catch(Exception e) {
-				context.log "Could not get DomainConfiguration.JMX.PlatformMBeanServer{Enabled,Used}", e
-			}
-			if (!enabled && !used) {
-				i 'PlatformMBeanServer probably needs to be enabled in your domain configuration to see the beans below.'
-			}
-			
-			def map = runtimeServer.getMBeanPlatformHome().properties.findAll { key, value ->
-				value instanceof MBean || value instanceof Collection<MBean>
-			}
-			
-			map.each { name, mbeans -> 
-				h3 name
-				if (mbeans instanceof MBean) {
-					def objectName = mbeans.@objectName
-					if (mbeans) {
-						a(href:"?objectName=$objectName") { pre objectName }
-					} else {
-						pre objectName
-					}
-				} else if (mbeans instanceof Collection<MBean>) {
-					ul {
-						mbeans.each { mbean ->
-							def objectName = mbean.@objectName
-							li {
-								if (mbean) {
-									a(href:"?objectName=$objectName") { pre objectName }
-								} else {
-									pre objectName
+	if (params.objectName) {
+		def home = runtimeServer.home
+		assert home
+		def mbean = home.getMBean(params.objectName)
+		assert mbean
+		
+		response.bufferSize = 350000
+		def htmlExporter = new HtmlExporter(response.writer)	
+		
+		// TODO any additional parameters or preferences
+		
+		def timestamp = new Date()
+		def extras = [
+					'URL':request.requestURL,
+					'Timestamp':timestamp,
+					'user':request.remoteUser, 
+					'principal':request.userPrincipal]
+		htmlExporter.mbean mbean, extras
+		
+	} else {
+		
+		html.html {
+			head { title 'GWLST RuntimeMBeanServer Browser' }
+			body {
+				h1 'GWLST RuntimeMBeanServer Browser'
+				h2 'MBeanHome'
+				pre runtimeServer.home
+				h2 'WebLogic Services'
+				for (name in [
+					'runtimeService',
+					'typeService'
+				]) {
+					def mbean = runtimeServer."$name"
+					def objectName = mbean.@objectName
+					h3 name
+					a(href:"?objectName=$objectName") { pre objectName }
+				}
+				
+				h2 'Java Platform MXBeans'
+				def used = null
+				def enabled = null
+				try {
+					def jmx = runtimeServer.runtimeService.DomainConfiguration.JMX
+					enabled = jmx.PlatformMBeanServerEnabled
+					used = jmx.PlatformMBeanServerUsed	// Only from 10.3.3 ?
+				}
+				catch(Exception e) {
+					context.log "Could not get DomainConfiguration.JMX.PlatformMBeanServer{Enabled,Used}: $e.message"
+				}
+				if (!enabled && !used) {
+					i 'PlatformMBeanServer probably needs to be enabled in your domain configuration to see the beans below.'
+				}
+				
+				def map = runtimeServer.getMBeanPlatformHome().properties.findAll { key, value ->
+					value instanceof MBean || value instanceof Collection<MBean>
+				}
+				
+				map.each { name, mbeans -> 
+					h3 name
+					if (mbeans instanceof MBean) {
+						def objectName = mbeans.@objectName
+						if (mbeans) {
+							a(href:"?objectName=$objectName") { pre objectName }
+						} else {
+							pre objectName
+						}
+					} else if (mbeans instanceof Collection<MBean>) {
+						ul {
+							mbeans.each { mbean ->
+								def objectName = mbean.@objectName
+								li {
+									if (mbean) {
+										a(href:"?objectName=$objectName") { pre objectName }
+									} else {
+										pre objectName
+									}
 								}
 							}
 						}
