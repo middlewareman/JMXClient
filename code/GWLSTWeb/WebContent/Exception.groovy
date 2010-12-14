5ae0e3ade1fabadca6096d0b60ecc270674cc49b
Andreas <Andreas@9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9>
2010-12-14 13:57:05 +0000

    Improved error handling: either by error code or by exception. ExceptionHandler lets groovlets try/catch/forward themselves.
    git-svn-id: svn://localhost/GroovyJMX/trunk@96 9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9

--- code/GWLSTWeb/WebContent/Exception.groovy
+++ code/GWLSTWeb/WebContent/Exception.groovy
@@ -12,29 +12,36 @@ import com.middlewareman.groovy.StackTraceCleaner
 
 String toString(Throwable t) {
 	def sw = new StringWriter()
-	sw.withPrintWriter { pw -> t.printStackTrace(pw) }
+	sw.withPrintWriter { pw ->
+		t.printStackTrace(pw)
+	}
 	return sw.toString()
 }
 
-Throwable t = request.'javax.servlet.error.exception' ?: new Throwable("Test Exception.groovy Throwable")
-def full = toString(t)
-int removed = new StackTraceCleaner().deepClean(t)
-def brief = toString(t)
-
 html.html {
-	head { title 'Groovy Exception' }
+	head { title 'GWLST Error' }
 	body {
-		h1 t.class.name
+		h2 'Status code'
+		pre request.'javax.servlet.error.status_code' ?: 'Not available'
+		h2 'Exception type'
+		pre request.'javax.servlet.error.exception_type'?.name ?: 'Not available' 
 		h2 'URI'
-		pre request.'javax.servlet.error.request_uri' ?: 'testURI'
+		pre request.'javax.servlet.error.request_uri' ?: 'Not available'
+		h2 'Servlet name'
+		pre request.'javax.servlet.error.servlet_name' ?: 'Not available'
 		h2 'Message'
-		pre request.'javax.servlet.error.message' ?: 'testMessage'
-		h2 'Exception brief'
-		div {
-			blockquote { pre brief }
-			div "Removed $removed stack trace elements"
+		pre request.'javax.servlet.error.message' ?: 'No message provided'
+		h2 'Exception'
+		Throwable t = request.'javax.servlet.error.exception'
+		if (t) {
+			int removed = new StackTraceCleaner().deepClean(t)
+			def brief = toString(t)
+			div {
+				blockquote { pre brief }
+				div "Removed $removed stack trace elements"
+			}
+		} else {
+			pre 'Not available'
 		}
-		h2 'Exception full'
-		blockquote { pre full }
 	}
 }
---
+++ code/GWLSTWeb/src/ExceptionHandler.groovy
@@ -0,0 +1,57 @@
+/*
+ * $Id$
+ * Copyright © 2010 Middlewareman Limited. All rights reserved.
+ */
+import javax.servlet.RequestDispatcher 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Handles forwarding to error page for groovlets that are not happy with GroovyServlet behaviour.
+ * 
+ * @author Andreas Nyberg
+ */
+class ExceptionHandler {
+	
+	/** 
+	 * Move values into places that the error page expects and forward.
+	 */
+	static void exception(Throwable t, HttpServletRequest request,
+	HttpServletResponse response, RequestDispatcher dispatcher) {
+		
+		//request.'javax.servlet.error.status_code'
+		request.'javax.servlet.error.exception_type' = t.getClass()
+		request.'javax.servlet.error.message' = t.message
+		request.'javax.servlet.error.request_uri' = request.requestURI
+		request.'javax.servlet.error.exception' = t
+		//request.'javax.servlet.error.servlet_name'
+		
+		dispatcher.forward request, response
+	}
+	
+	private binding;
+	private errorPage;
+	
+	ExceptionHandler(Binding binding, String errorPage = 'Exception.groovy') {
+		this.binding = binding;
+		this.errorPage = errorPage;
+	}
+	
+	void exception(Throwable t) {
+		HttpServletRequest request = binding.request
+		assert request
+		HttpServletResponse response = binding.response
+		assert response
+		RequestDispatcher dispatcher = request.getRequestDispatcher(errorPage)
+		assert dispatcher
+		exception t, request, response, dispatcher
+	}
+	
+	void wrap(Closure script) {
+		try {
+			script.call()
+		} catch (Exception e) {
+			exception e
+		}
+	}
+}
