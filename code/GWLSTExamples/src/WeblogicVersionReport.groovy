7f5a16ce27a7b6ac88f34dafaa353eea5461005e
Andreas <Andreas@9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9>
2010-12-16 15:29:39 +0000

    Added per cluster loop.
    git-svn-id: svn://localhost/GroovyJMX/trunk@137 9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9

--- code/GWLSTExamples/src/WeblogicVersionReport.groovy
+++ code/GWLSTExamples/src/WeblogicVersionReport.groovy
@@ -2,6 +2,7 @@
 /**
  * Reports the WebLogic version reported by running servers in a domain
  * grouped by version.
+ * Note that version can only be queried from running servers.
  * 
  * @author Andreas Nyberg
  */
@@ -10,17 +11,40 @@ println status()
 
 def drs = domainRuntimeServer.domainRuntimeService
 
-def map = [:]
+println "\nDOMAIN $drs.domainConfiguration.name"
+
+def domainMap = [:]
 for (serverRuntime in drs.serverRuntimes) 
-	map[serverRuntime.name] = serverRuntime.weblogicVersion
+	domainMap[serverRuntime.name] = serverRuntime.weblogicVersion
 
-def reverseMap = map.groupBy { name, version -> version } 
+def domainReverseMap = domainMap.groupBy { name, version -> version } 
 
-def domainLevels = reverseMap.size()
+def domainLevels = domainReverseMap.size()
 if (domainLevels > 1)
 	println "WARNING: There are $domainLevels different versions/patchlevels in your DOMAIN."
-for (version in reverseMap.keySet()) {
-	def nameSet = reverseMap[version].keySet().sort()
-	println "\n$nameSet:"
+for (version in domainReverseMap.keySet()) {
+	def nameSet = domainReverseMap[version].keySet().sort()
+	println "\nServer $nameSet:"
 	println version
+}
+
+for (cluster in drs.domainConfiguration.clusters) {
+	def clusterName = cluster.name
+	println "\nCLUSTER $clusterName"
+	def configNames = cluster.servers?.name as Set
+	def runtimeNames = domainMap.keySet()
+	def unavailableNames = configNames - runtimeNames
+	if (unavailableNames) 
+		println "Warning: cannot check cluster members not running: $unavailableNames"
+	def clusterMap = domainMap.findAll { key, value -> key in runtimeNames }
+	def clusterReverseMap = clusterMap.groupBy { name, version -> version }
+	def clusterLevels = domainReverseMap.size()
+	if (clusterLevels > 1) {
+		println "WARNING: There are $clusterLevels different versions/patchlevels in cluster $clusterName."
+		for (version in clusterReverseMap.keySet()) {
+			def nameSet = domainReverseMap[version].keySet().sort()
+			println "\nServer $nameSet:"
+			println version
+		}
+	}
 }
