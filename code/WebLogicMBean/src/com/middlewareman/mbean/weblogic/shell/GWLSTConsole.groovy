209c2b378ce4372edf4103a3d5832180c32d90d2
Andreas <Andreas@9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9>
2010-12-16 02:28:26 +0000

    Console doesn't take arguments
    git-svn-id: svn://localhost/GroovyJMX/trunk@130 9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9

--- code/WebLogicMBean/src/com/middlewareman/mbean/weblogic/shell/GWLSTConsole.groovy
+++ code/WebLogicMBean/src/com/middlewareman/mbean/weblogic/shell/GWLSTConsole.groovy
@@ -11,17 +11,14 @@ class GWLSTConsole {
 	
 	static void main(String[] args) {
 		def hf = WebLogicMBeanHomeFactory.default
+		hf.loadEnvironmentProperties()
+		hf.loadSystemProperties()
+	
 		args = hf.loadArguments(args)
-		File file
-		if (args) {
-			file = new File(args[0])
-			assert file.exists(), "First argument file $file does not exist"
-			args = args[1..-1]
-		}
+
 		def binding = new Binding()
 		GWLSTBindings.bind(hf,binding)
 		def console = new Console(binding)
 		console.run()
-		if (file) console.loadScriptFile(file)
 	}
 }
