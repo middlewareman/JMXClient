6e38aa7cd03b6695ed4d00bb185feb4005d01ffc
Andreas <Andreas@9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9>
2011-01-03 22:56:02 +0000

    Time consuming recursive test removed as superseded by MBeanIterator.
    git-svn-id: svn://localhost/GroovyJMX/trunk@186 9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9

--- code/WebLogicMBean/test/com/middlewareman/mbean/weblogic/builder/HtmlExporterTest.groovy
+++ code/WebLogicMBean/test/com/middlewareman/mbean/weblogic/builder/HtmlExporterTest.groovy
@@ -1,6 +1,6 @@
 /*
  * $Id$
- * Copyright © 2010 Middlewareman Limited. All rights reserved.
+ * Copyright (c) 2010 Middlewareman Limited. All rights reserved.
  */
 package com.middlewareman.mbean.weblogic.builder
 
@@ -37,47 +37,6 @@ class HtmlExporterTest extends GroovyTestCase {
 		namedFile.withPrintWriter {
 			def htmle = new HtmlExporter(it)
 			htmle.mbean domain.info
-		}
-	}
-	
-	void testServerRuntimeRecursive() {
-		def sr = rs.runtimeService.ServerRuntime
-		File file = namedFile
-		long start = System.currentTimeMillis()
-		file.withPrintWriter { 
-			def htmle = new HtmlExporter(it)
-			recurse(sr,htmle,new LinkedHashSet<ObjectName>())
-		}
-		long finish = System.currentTimeMillis()
-		println "$name wrote ${file.length()} bytes to $file.name in ${finish - start} ms."
-	}
-	
-	void recurse(MBean mbean, HtmlExporter he, Set<ObjectName> visited) {
-		if (mbean == null || mbean.@objectName in visited) {
-			return
-		}
-		visited.add mbean.@objectName
-		he.mbean mbean
-		def ais = mbean.info.attributes
-		for (ai in ais) {
-			if (ai.type.contains('javax.management.ObjectName')) {
-				try {
-					def child = mbean."$ai.name"
-					if (child != null) {
-						if (child instanceof MBean) {
-							recurse child, he, visited
-						} else if (child instanceof MBean[] || child instanceof Collection<MBean>) {
-							for (mb in child) 
-								recurse mb, he, visited
-						} else {
-							System.err.println "ignoring $child"
-						}
-					}
-				} catch(Exception e) {
-					System.err.println "$name $ai.name $ai.type failed on $mbean"
-					e.printStackTrace()
-				}
-			}
 		}
 	}
 	
