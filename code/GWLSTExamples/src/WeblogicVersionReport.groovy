b0d24958c8edd47a3955815e34b792aa8e6ac5aa
Andreas <Andreas@9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9>
2011-01-04 18:07:34 +0000

    Cleaned and improved; reports only domain and not per cluster.
    git-svn-id: svn://localhost/GroovyJMX/trunk@197 9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9

--- code/GWLSTExamples/src/WeblogicVersionReport.groovy
+++ code/GWLSTExamples/src/WeblogicVersionReport.groovy
@@ -7,45 +7,30 @@
  * @author Andreas Nyberg
  */
 
-println status()
-println new Date()
+def ipw = new com.middlewareman.groovy.util.IndentPrintWriter()
+ipw.println status()
+ipw.println new Date()
+ipw.println()
 
 def drs = domainRuntimeServer.domainRuntimeService
 
-println "\nDOMAIN $drs.domainConfiguration.name"
+def notRunning =
+		(drs.DomainConfiguration.Servers.Name as Set) -
+		(drs.ServerRuntimes.Name as Set)
+if (notRunning)
+	ipw.println "WARNING: The following servers are not available to be queried: $notRunning\n"
 
 def domainMap = [:]
-for (serverRuntime in drs.serverRuntimes) 
-	domainMap[serverRuntime.name] = serverRuntime.weblogicVersion
+for (serverRuntime in drs.ServerRuntimes)
+	domainMap[serverRuntime.name] = serverRuntime.WeblogicVersion
 
-def domainReverseMap = domainMap.groupBy { name, version -> version } 
+def domainReverseMap = domainMap.groupBy { name, version -> version }
 
 def domainLevels = domainReverseMap.size()
 if (domainLevels > 1)
-	println "WARNING: There are $domainLevels different versions/patchlevels in your DOMAIN."
+	ipw.println "WARNING: There are $domainLevels different patchlevels in your domain.\n"
+
 for (version in domainReverseMap.keySet()) {
 	def nameSet = domainReverseMap[version].keySet().sort()
-	println "\nServer $nameSet:"
-	println version
-}
-
-for (cluster in drs.domainConfiguration.clusters) {
-	def clusterName = cluster.name
-	println "\nCLUSTER $clusterName"
-	def configNames = cluster.servers?.name as Set
-	def runtimeNames = domainMap.keySet()
-	def unavailableNames = configNames - runtimeNames
-	if (unavailableNames) 
-		println "Warning: cannot check cluster members not running: $unavailableNames"
-	def clusterMap = domainMap.findAll { key, value -> key in runtimeNames }
-	def clusterReverseMap = clusterMap.groupBy { name, version -> version }
-	def clusterLevels = domainReverseMap.size()
-	if (clusterLevels > 1) {
-		println "WARNING: There are $clusterLevels different versions/patchlevels in cluster $clusterName."
-		for (version in clusterReverseMap.keySet()) {
-			def nameSet = domainReverseMap[version].keySet().sort()
-			println "\nServer $nameSet:"
-			println version
-		}
-	}
+	ipw.indent("\nServers $nameSet:") { ipw.println version }
 }
