398ea0d0d8bd5575fadfcc7c91e1dd01b7811779
Andreas <Andreas@9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9>
2011-01-10 12:21:46 +0000

    git-svn-id: svn://localhost/GroovyJMX/trunk@210 9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9

---
+++ code/GWLSTExamples/scripts/jvm/ThreadDump.groovy
@@ -0,0 +1,26 @@
+/* $Id$ */
+/**
+ * Pull a thread dump for WebLogic 10.0.
+ * If admin server, you may name servers to collect, or all will be collected.
+ * Otherwise, only connected server will be returned.
+ * 
+ * @author Andreas Nyberg
+ */
+
+if (domainRuntimeServer) {
+	println "Connected to admin server"
+	def drs = domainRuntimeServer.domainRuntimeService
+	def servers = args ? args.collect { drs.lookupServerRuntime it } : drs.ServerRuntimes
+	for (server in servers) {
+		if (server) {
+			println "\nServer $server.@objectName on $server.@home\n"
+			println server.JVMRuntime.ThreadStackDump
+		} else {
+			println "\nServer $serverName not found"
+		}
+	}
+} else {
+	println "Connected to managed server"
+	def server = runtimeServer.runtimeService.ServerRuntime
+	println server.JVMRuntime.ThreadStackDump
+}
