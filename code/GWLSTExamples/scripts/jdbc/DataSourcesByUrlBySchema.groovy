ab26af57d7bf6ed5ab7b1dc1d74849c4f03c2192
Andreas <Andreas@9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9>
2011-01-26 12:58:08 +0000

    Handles defunct JDBCSystemResource and MultiDataSources.
    git-svn-id: svn://localhost/GroovyJMX/trunk@274 9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9

--- code/GWLSTExamples/scripts/jdbc/DataSourcesByUrlBySchema.groovy
+++ code/GWLSTExamples/scripts/jdbc/DataSourcesByUrlBySchema.groovy
@@ -7,45 +7,55 @@ ipw.println new Date()
 ipw.println domainRuntimeServer.home
 ipw.println()
 
-def jdbcsList = domainRuntimeService.DomainConfiguration.JDBCSystemResources as List
+// Filter out defunct JDBCSystemResource without JDBCResource
+def jdbcsList = domainRuntimeService.DomainConfiguration.JDBCSystemResources.findAll { it.JDBCResource }
+
+// TODO: Split simple and multi datasource here rather than getting null Url and User
+
 def jdbcsByUrl = jdbcsList.groupBy {
-	it.JDBCResource.JDBCDriverParams.Url
+	it.JDBCResource?.JDBCDriverParams?.Url
 }
 
 jdbcsByUrl.each { url, jdbcsPerUrl ->
 	ipw.indent("\nURL $url") {
+		assert jdbcsPerUrl != null
 		def jdbcsByUser = jdbcsPerUrl.groupBy {
-			it.JDBCResource.JDBCDriverParams.Properties.Properties.find { it.Name == 'user' }.Value
+			it.JDBCResource?.JDBCDriverParams?.Properties?.Properties?.find { it?.Name == 'user' }?.Value
 		}
 		jdbcsByUser.each { user, jdbcsPerUser ->
 			ipw.indent("\nUSER $user") {
 				for (jdbc in jdbcsPerUser) {
 					def jdbcName = jdbc.Name
-					def initial = jdbc.JDBCResource.JDBCConnectionPoolParams.InitialCapacity
-					def max = jdbc.JDBCResource.JDBCConnectionPoolParams.MaxCapacity
-					def jndiNames = jdbc.JDBCResource.JDBCDataSourceParams.JNDINames
-					ipw.indent("\nJDBC $jdbcName\t$initial\t$max\t$jndiNames") {
+					def resource = jdbc.JDBCResource
+					def pool = resource.JDBCConnectionPoolParams
+					def datasource = resource.JDBCDataSourceParams
+					def initial = pool.InitialCapacity
+					def max = pool.MaxCapacity
+					def jndiNames = datasource.JNDINames
+					def dataSourceList = datasource.DataSourceList
+					ipw.indent("\nJDBC $jdbcName\t$initial\t$max\t$jndiNames\t$dataSourceList") {
 						for (target in jdbc.Targets) {
 							def name = target.@objectName.getKeyProperty('Name')
 							switch(target.@objectName.getKeyProperty('Type')) {
 								case 'Server':
-									ipw.indent("\nSERVER $name") {
-										//def datasourceRuntime = domainRuntimeServer.home.getMBean("com.bea:Name=$jdbcName,ServerRuntime=$name,Location=$name,Type=JDBCDataSourceRuntime")
-										def serverRuntime = domainRuntimeService.lookupServerRuntime(name)
-										if (serverRuntime) {
-											def jdbcServiceRuntime = serverRuntime.JDBCServiceRuntime
-											def dsRuntime = jdbcServiceRuntime.JDBCDataSourceRuntimeMBeans.find { it.Name == jdbcName }
-											if (dsRuntime) {
-												ipw.println "ActiveHigh \t$dsRuntime.ActiveConnectionsHighCount"
-												ipw.println "CurrentHigh\t$dsRuntime.CurrCapacityHighCount"
-												ipw.println "WaitSecHigh\t$dsRuntime.WaitSecondsHighCount"
-												ipw.println "WaitingHigh\t$dsRuntime.WaitingForConnectionHighCount"
-											} else {
-												ipw.println "No JDBCDataSourceRuntime"
-											}
+									ipw.print("\nSERVER $name\t")
+									def serverRuntime = domainRuntimeService.lookupServerRuntime(name)
+									if (serverRuntime) {
+										def jdbcServiceRuntime = serverRuntime.JDBCServiceRuntime
+										def dsRuntime = jdbcServiceRuntime.JDBCDataSourceRuntimeMBeans.find { it?.Name == jdbcName }
+										if (dsRuntime) {
+											ipw.print dsRuntime.ActiveConnectionsHighCount
+											ipw.print '\t'
+											ipw.print dsRuntime.CurrCapacityHighCount
+											ipw.print '\t'
+											ipw.print dsRuntime.WaitSecondsHighCount
+											ipw.print '\t'
+											ipw.println dsRuntime.WaitingForConnectionHighCount
 										} else {
-											ipw.println "No ServerRuntime"
+											ipw.println "No JDBCDataSourceRuntime"
 										}
+									} else {
+										ipw.println "No ServerRuntime"
 									}
 									break
 								case 'Cluster':
