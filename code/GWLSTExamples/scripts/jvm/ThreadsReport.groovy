81ecf751ca4c4b8a047921ca5a14566376cdf63f
Andreas <Andreas@9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9>
2011-01-31 16:41:20 +0000

    git-svn-id: svn://localhost/GroovyJMX/trunk@286 9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9

---
+++ code/GWLSTExamples/scripts/jvm/ThreadsReport.groovy
@@ -0,0 +1,17 @@
+/* $Id$ */
+/**
+ * Loop through server runtimes and report thread usage.
+ * @author Andreas Nyberg
+ */
+def serverNameToRuntime = [:]
+domainRuntimeService.ServerRuntimes.each { serverNameToRuntime[it.Name] = it }
+def serverNames = serverNameToRuntime.keySet().sort()
+
+println "NAME\tSTARTED\tCURRENT\tPEAK\tCPUs\tMACHINE"
+for (serverName in serverNames) {
+    def ph = domainRuntimeServer.getProxyPlatformHome(serverName)
+    def thread = ph.thread
+    def os = ph.operatingSystem
+    def machine = serverNameToRuntime[serverName].CurrentMachine
+    println "$serverName\t$thread.totalStartedThreadCount\t$thread.threadCount\t$thread.peakThreadCount\t$os.availableProcessors\t$machine"
+}
