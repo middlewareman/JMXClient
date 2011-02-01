1cbcc8551891722ea68cab6706e149b4aacf690a
Andreas <Andreas@9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9>
2011-02-01 17:29:15 +0000

    git-svn-id: svn://localhost/GroovyJMX/trunk@292 9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9

--- code/GWLSTExamples/scripts/jvm/CollectThreadDumps.groovy
+++ code/GWLSTExamples/scripts/jvm/CollectThreadDumps.groovy
@@ -1,12 +1,21 @@
 /* $Id$ */
 def df = new java.text.SimpleDateFormat('yyyyMMdd-HHmmss')
 def domainName = domainRuntimeService.DomainConfiguration.Name
-def timestamp = df.format(new Date())
-def root = new File("${domainName}/${timestamp}")
-root.mkdirs()
-for (sr in domainRuntimeService.ServerRuntimes) {
-	def serverName = sr.Name
-	println serverName
-	String text = sr.JVMRuntime.ThreadStackDump
-	new File(root,"${sr.Name}.txt").write(text)
+while(true) {
+	def timestamp = df.format(new Date())
+	println timestamp
+	def root = new File("${domainName}/${timestamp}")
+	root.mkdirs()
+	for (sr in domainRuntimeService.ServerRuntimes) {
+		def serverName = sr.Name
+		println serverName
+		def ph = domainRuntimeServer.getProxyPlatformHome(serverName)
+		def thread = ph.thread
+		String text = sr.JVMRuntime.ThreadStackDump
+		boolean stuck = text.contains('STUCK')
+		def name = "${sr.Name}-${thread.threadCount}"
+		if (stuck) name += "-STUCK"
+		new File(root,name + '.txt').write(text)
+	}
+	Thread.sleep 30000
 }
