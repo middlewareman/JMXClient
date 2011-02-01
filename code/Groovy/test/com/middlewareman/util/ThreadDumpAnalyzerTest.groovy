ce0ffe6440d87f790a1cb0d6d8d8cb0336ea99bb
Andreas <Andreas@9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9>
2011-02-01 09:05:12 +0000

    Some more basic testing without aggregation.
    git-svn-id: svn://localhost/GroovyJMX/trunk@288 9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9

--- code/Groovy/test/com/middlewareman/util/ThreadDumpAnalyzerTest.groovy
+++ code/Groovy/test/com/middlewareman/util/ThreadDumpAnalyzerTest.groovy
@@ -9,14 +9,19 @@ class ThreadDumpAnalyzerTest extends GroovyTestCase {
 		def sw = new StringWriter()
 		ManagementFactory.threadMXBean.dumpAllThreads(true,true).each { sw.println it }
 		String text = sw.toString()
+		println name
+		println text
 		assert text
 		def tda = new ThreadDumpAnalyzer()
 		tda.parse text
+		println tda.parsed.size()
 		assert tda.parsed
+		println tda.parsed[0]
 		assert tda.parsed.any { it.trace }
 	}
 
 	void testJava5Snippet() {
+		println "*** $name ***"
 		def text = '''
 "[STANDBY] ExecuteThread: '28' for queue: 'weblogic.kernel.Default (self-tuning)'" daemon prio=10 tid=0x029e3b98 nid=0x11ba in Object.wait() [0x457ff000..0x457ff970]
 	at java.lang.Object.wait(Native Method)
@@ -29,9 +34,8 @@ class ThreadDumpAnalyzerTest extends GroovyTestCase {
 '''
 		def tda = new ThreadDumpAnalyzer()
 		tda.parse text
-		println name
 		println text
-		println tda.parsed
+		println tda.parsed.name
 		assert tda.parsed.size() == 1
 		println tda.parsed[0]
 		println tda.parsed[0].trace
@@ -39,15 +43,19 @@ class ThreadDumpAnalyzerTest extends GroovyTestCase {
 	}
 
 	void testJava5Full() {
+		println "*** $name ***"
 		String text = new File('Java5FullThreadDump.txt').text
 		assert text
 		def tda = new ThreadDumpAnalyzer()
 		tda.parse text
+		println tda.parsed.size()
 		assert tda.parsed
+		println tda.parsed[0]
 		assert tda.parsed.any { it.trace }
 	}
 
 	void testWLS1002Snippet() {
+		println "*** $name ***"
 		def text = '''        
           
             "[STANDBY] ExecuteThread: '236' for queue: 'weblogic.kernel.Default (self-tuning)'" waiting for lock weblogic.work.ExecuteThread@7e7702 WAITING
@@ -63,9 +71,7 @@ class ThreadDumpAnalyzerTest extends GroovyTestCase {
             '''
 		def tda = new ThreadDumpAnalyzer()
 		tda.parse text
-		println name
 		println text
-		println tda.parsed
 		assert tda.parsed.size() == 1
 		println tda.parsed[0]
 		println tda.parsed[0].trace
@@ -73,11 +79,14 @@ class ThreadDumpAnalyzerTest extends GroovyTestCase {
 	}
 
 	void testWLS1002() {
+		println "*** $name ***"
 		String text = new File('WLS1002CopyPastedThreadDump.txt').text
 		assert text
 		def tda = new ThreadDumpAnalyzer()
 		tda.parse text
+		println tda.parsed.size()
 		assert tda.parsed
+		println tda.parsed[0]
 		assert tda.parsed.any{ it.trace }
 	}
 }
