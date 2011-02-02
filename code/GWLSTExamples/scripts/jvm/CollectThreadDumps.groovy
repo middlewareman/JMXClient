287aec268de4d52edc740c05180dc511756b07d7
Andreas <Andreas@9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9>
2011-02-02 15:39:35 +0000

    git-svn-id: svn://localhost/GroovyJMX/trunk@300 9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9

--- code/GWLSTExamples/scripts/jvm/CollectThreadDumps.groovy
+++ code/GWLSTExamples/scripts/jvm/CollectThreadDumps.groovy
@@ -11,11 +11,26 @@ while(true) {
 		println serverName
 		def ph = domainRuntimeServer.getProxyPlatformHome(serverName)
 		def thread = ph.thread
-		String text = sr.JVMRuntime.ThreadStackDump
+		def jvm = sr.JVMRuntime
+		String text = jvm.ThreadStackDump
 		boolean stuck = text.contains('STUCK')
-		def name = "${sr.Name}-${thread.threadCount}"
+		def name = "${sr.Name}-${timestamp}-${thread.threadCount}"
 		if (stuck) name += "-STUCK"
 		new File(root,name + '.txt').write(text)
+		try {
+			def tda = new com.middlewareman.util.ThreadDumpAnalyzer()
+			tda.parse text
+			new File(root,name + '.analysed.txt').withPrintWriter { out ->
+				out.println sr
+				out.println "FreeCur\tFree%\tSizeCur\tSizeMax"
+				out.println "${jvm.HeapFreeCurrent>>20} MB\t${jvm.HeapFreePercent}%\t${jvm.HeapSizeCurrent>>20} MB\t${jvm.HeapSizeMax>>20} MB"
+				out.println()
+				tda.report out
+			}
+		} catch(StackOverflowError e) {
+			println "StackOverflowError for $serverName"
+		}
 	}
+	println 'Sleeping...'
 	Thread.sleep 30000
 }
