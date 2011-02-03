44f50cc6e4025a4c3ff685eb9cf916d0f4a31307
Andreas <Andreas@9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9>
2011-02-03 17:50:40 +0000

    Fix related to earlier change in HtmlExporter.
    git-svn-id: svn://localhost/GroovyJMX/trunk@307 9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9

--- code/GWLSTWeb/WebContent/DomainRuntimeBrowser.groovy
+++ code/GWLSTWeb/WebContent/DomainRuntimeBrowser.groovy
@@ -46,7 +46,7 @@ new ExceptionHandler(binding).wrap {
 
 		// TODO any additional parameters or preferences
 
-		htmlExporter.mbean typeService, interfaceClassName, info, subtypes
+		htmlExporter.mbean interfaceClassName, info, subtypes
 
 	} else {
 
