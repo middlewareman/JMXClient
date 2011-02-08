7488dcae4a383ccea118b3eab4e418cf6f9104fb
Andreas <Andreas@9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9>
2011-02-08 16:03:05 +0000

    git-svn-id: svn://localhost/GroovyJMX/trunk@331 9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9

---
+++ code/GWLSTExamples/scripts/nodemanager/ExportProperties.groovy
@@ -0,0 +1,11 @@
+/* $Id$ */
+def domain = domainRuntimeService.DomainConfiguration
+String serverName = args.length ? args[0] : domain.AdminServerName
+def server = domain.lookupServer(serverName)
+assert server, "Could not look up server $serverName"
+new File('startup.properties').withOutputStream {
+	server.ServerStart.StartupProperties.store(it,"Exported by GWLST from $server on ${server.@home.address}")
+}
+new File('boot.properties').withOutputStream {
+	server.ServerStart.BootProperties.store(it,"Exported by GWLST from $server on ${server.@home.address}")
+}
