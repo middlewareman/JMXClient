b530638378852ee2fee3eab24c29abf32da27d2f
Andreas <Andreas@9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9>
2011-02-09 16:11:30 +0000

    5 MB limit
    git-svn-id: svn://localhost/GroovyJMX/trunk@345 9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9

--- code/GWLSTExamples/scripts/logging/ConfigureTimingWebServerLog.groovy
+++ code/GWLSTExamples/scripts/logging/ConfigureTimingWebServerLog.groovy
@@ -9,6 +9,7 @@ editSave { domain ->
 	for (server in domain.Servers) {
 		def wslog = server.WebServer.WebServerLog
 		wslog.FileName = "../../logs/servers/$server.Name/access.log" as String
+		wslog.FileMinSize = 5<<10 // 5 MB instead of default 500 KB
 		wslog.LogFileFormat = 'extended'
 		wslog.ELFFields = 'date time cs-method cs-uri-stem time-taken cs-status bytes cs-uri-query'
 	}
