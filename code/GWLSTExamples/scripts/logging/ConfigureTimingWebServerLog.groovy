5aed249a35bba261475048757f439543b0ae67e6
Andreas <Andreas@9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9>
2011-02-09 09:32:53 +0000

    git-svn-id: svn://localhost/GroovyJMX/trunk@338 9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9

---
+++ code/GWLSTExamples/scripts/logging/ConfigureTimingWebServerLog.groovy
@@ -0,0 +1,15 @@
+/* $Id$ */
+/**
+ * Configures time-taken webserver (http) log.
+ * 
+ * @author Andreas Nyberg
+ */
+editSave { domain ->
+	def domainRootDir = domain.RootDirectory
+	for (server in domain.Servers) {
+		def wslog = server.WebServer.WebServerLog
+		wslog.FileName = "$domain.RootDirectory/logs/servers/$server.Name/access.log" as String
+		wslog.LogFileFormat = 'extended'
+		wslog.ELFFields = 'date time cs-method cs-uri-stem time-taken cs-status bytes cs-uri-query'
+	}
+}
