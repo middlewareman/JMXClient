3f8bb28e577f8662fa0b558e24a8a4a140c400de
Andreas <Andreas@9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9>
2011-02-25 10:03:26 +0000

    Add to binding: serverConfig, serverRuntime, domainConfig, domainRuntime
    git-svn-id: svn://localhost/GroovyJMX/trunk@393 9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9

--- code/WebLogicMBean/src/com/middlewareman/mbean/weblogic/shell/GWLSTBindings.groovy
+++ code/WebLogicMBean/src/com/middlewareman/mbean/weblogic/shell/GWLSTBindings.groovy
@@ -8,14 +8,22 @@ import com.middlewareman.mbean.weblogic.*
 
 /**
  * Create standard bindings for GWLST script.
- * <p>Beans:
+ * <p>For any WebLogic Server:
  * <ul>
  * <li><code>runtimeServer</code></li>
  * <li><code>runtimeService</code> (short for 
  * <code>runtimeServer.runtimeService</code>)</li>
+ * <li><code>serverConfig</code> (short for <code>runtimeService.ServerConfiguration</code></li>
+ * <li><code>serverRuntime</code> (short for <code>runtimeService.ServerRuntime</code></li>
+ * </ul>
+ * </p>
+ * <p>For an admin server:
+ * <ul>
  * <li><code>domainRuntimeServer</code></li>
  * <li><code>domainRuntimeService</code> (short for 
  * <code>domainRuntimeServer.domainRuntimeService</code>)</li>
+ * <li><code>domainConfig</code> (short for <code>domainRuntimeService.DomainConfiguration</code></li>
+ * <li><code>domainRuntime</code> (short for <code>domainRuntimeService.DomainRuntime</code></li>
  * <li><code>editServer</code></li>
  * <li><code>editService</code> (short for 
  * <code>editServer.editService</code>)</li>
@@ -24,8 +32,11 @@ import com.middlewareman.mbean.weblogic.*
  * <p>Closures:
  * <ul>
  * <li><code>String status()</code></li>
- * <li><code>edit(Closure script)</code> that passes the pending domain MBean 
- * as the parameter to the script</li>
+ * <li><code>editValidate(Closure script)</code></li>
+ * <li><code>editSave(Closure script)</code></li>
+ * <li><code>editActivateWait(Closure script)</code></li>
+ * <li><code>editActivateNoWait(Closure script)</code></li>
+ * <li>
  * </ul>
  * </p>
  * 
@@ -69,18 +80,22 @@ class GWLSTBindings {
 		/* If we got this far we have managed to connect! */
 		target.runtimeServer = newRuntimeServer
 		target.runtimeService = newRuntimeService
-		target.domainRuntimeServer = null
-		target.domainRuntimeService = null
-		target.editServer = null
-		target.editService = null
+		target.serverConfig = newRuntimeService.ServerConfiguration
+		target.serverRuntime = newRuntimeService.ServerRuntime
 		if (isAdminServer) {
 			def newDomainRuntimeServer = new DomainRuntimeServer(hf)
 			def newDomainRuntimeService = newDomainRuntimeServer.domainRuntimeService
 			if (newDomainRuntimeService) {
 				target.domainRuntimeServer = newDomainRuntimeServer
 				target.domainRuntimeService = newDomainRuntimeService
+				target.domainConfig = newDomainRuntimeService.DomainConfiguration
+				target.domainRuntime = newDomainRuntimeService.DomainRuntime
 			} else {
 				// TODO Warning?
+				target.domainRuntimeServer = null
+				target.domainRuntimeService = null
+				target.domainConfig = null
+				target.domainRuntime = null
 			}
 			def newEditServer = new EditServer(hf)
 			def newEditService = newEditServer.editService
@@ -89,6 +104,8 @@ class GWLSTBindings {
 				target.editService = newEditService
 			} else {
 				// TODO Warning?
+				target.editServer = null
+				target.editService = null
 			}
 		}
 		target.status = { status(target.runtimeService) }
