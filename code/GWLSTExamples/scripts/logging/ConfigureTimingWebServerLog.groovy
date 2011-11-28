739a75638ebe08501b5b6005296a7387300a164a
Andreas <Andreas@9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9>
2011-11-28 05:44:53 +0000

    Better absolute location and rotate on startup.
    git-svn-id: svn://localhost/JMXClient/Groovy/trunk@441 9f60ad4c-3a5e-0a40-8eef-3ec19c1eeff9

--- code/GWLSTExamples/scripts/logging/ConfigureTimingWebServerLog.groovy
+++ code/GWLSTExamples/scripts/logging/ConfigureTimingWebServerLog.groovy
@@ -5,12 +5,14 @@
  * @author Andreas Nyberg
  */
 editSave { domain ->
+	String domainName = domain.Name
 	for (server in domain.Servers) {
-		def serverName = server.Name
+		String serverName = server.Name
 		def wslog = server.WebServer.WebServerLog
-		wslog.FileName = "../../logs/servers/${serverName}/access-${serverName}.log" as String
+		wslog.FileName = "/apps/bea/LOGS/${domainName}/${serverName}/access-${domainName}-${serverName}.log" as String
 		wslog.FileMinSize = 5<<10 // 5 MB instead of default 500 KB
 		wslog.LogFileFormat = 'extended'
 		wslog.ELFFields = 'date time cs-method cs-uri-stem time-taken cs-status bytes cs-uri-query'
+		wslog.RotateLogOnStartup = true
 	}
 }
